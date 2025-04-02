8979f0e8d26548b17b4a4b95fd0a79b8011457dc
Kuroi Kyu <hello@kuroikyu.com>
2025-04-02 20:51:57 +0200

    chore: install prettier and run it through the files

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -3,20 +3,17 @@ import { Navigate, Outlet } from "react-router";
 
 import { useAppSelector } from "@/store/hooks";
 
-
 const AuthLayout: FC = () => {
-  const { isAuthenticated, loading } = useAppSelector(state => state.auth)
+  const { isAuthenticated, loading } = useAppSelector((state) => state.auth);
 
   if (isAuthenticated && !loading) {
-    return <Navigate to="/" replace />
+    return <Navigate to="/" replace />;
   }
   return (
-
     <div className="flex min-h-screen items-center justify-center bg-gray-50 px-4">
       <Outlet />
     </div>
-  )
-}
+  );
+};
 
-
-export default AuthLayout
+export default AuthLayout;
