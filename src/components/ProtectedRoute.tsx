c75b3d51d02cae2f25968b2a1897fc314b4c1c90
Kuroi Kyu <hello@kuroikyu.com>
2025-04-04 13:09:19 +0200

    refactor: get user reading stats on when accessing a protected route area

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,13 +1,22 @@
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useEffect } from "react";
 import { Navigate } from "react-router";
-import { useAppSelector } from "@/store/hooks";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { fetchBookStatsByUser } from "@/store/bookStatsSlice";
 
 interface ProtectedRouteProps {
   children: ReactNode;
 }
 
 const ProtectedRoute: FC<ProtectedRouteProps> = ({ children }) => {
-  const { isAuthenticated } = useAppSelector((state) => state.auth);
+  const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
+  const user = useAppSelector((state) => state.auth.user);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (user) {
+      dispatch(fetchBookStatsByUser(user.id));
+    }
+  }, [dispatch, user]);
 
   if (!isAuthenticated) {
     return <Navigate to="/login" />;
